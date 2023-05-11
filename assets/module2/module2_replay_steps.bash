8057db35c91bd566b188594134f414be3e7f1be6
student_04_a0dae6586f82 <student_04_a0dae6586f82@qwiklabs.net>
2023-05-11 20:49:44 +0000

    Source previous steps and fix issues

--- assets/module2/module2_replay_steps.bash
+++ assets/module2/module2_replay_steps.bash
@@ -8,24 +8,33 @@ script_name=$(basename "$0")
 #shellcheck disable=SC2128,SC2034
 script_dir=$(dirname "$(readlink --canonicalize --no-newline "$0")")
 source "$script_dir/../common/common_bash_libs"
+#shellcheck disable=SC1091
+source "$HOME/.labenv_custom.bash"
 
 # Check that cloudshell bootstrap has been done
 check_cloudshell_bootstrap() {
-  [[ -f "$HOME/.config/cnd/.cloudshell_bootstrap.done" ]] || { error "Cloudshell bootstrap execution has not been properly registered. Aborting." && exit 1; }
+  [[ -f "$HOME/.config/cnd/.cloudshell_bootstrap.done" ]] || {
+    info "Bootstrapping basic Cloud Shell configuration..."
+    "$script_dir/../common/cloudshell_bootstrap.bash"
+  }
 }
 
 # Check that module1 steps replay has been done
 check_module1_bootstrap() {
-  [[ -f "$HOME/.config/cnd/.module1_steps.done" ]] || { error "Module 1 replay steps have not been properly registered. Aborting." && exit 1; }
+  [[ -f "$HOME/.config/cnd/.module1_steps.done" ]] || { 
+    info "Replaying Module 1 steps..."
+    "$script_dir/../module1/module1_replay_steps.bash"
+  }
 }
 
 ## Copy the Docker assets to the app dir
 set_docker_assets_and_deps() {
+  info "Copying Docker assets and basic app dependencies..."
   local -r cp_source="$script_dir"
   cp -- "$cp_source/Dockerfile" \
     "$cp_source/.dockerignore" \
     "$cp_source/requirements.txt" \
-    "$WORKDIR/myfirstapp"
+    "$script_dir/../../myfirstapp"
 }
 
 ## Set GCP services
@@ -45,9 +54,9 @@ create_gcp_services() {
 
 ## Deploy the app to Cloud Run
 deploy_to_cloudrun() {
-  pushd "$WORKDIR/myfirstapp" || { error "Failed to move to dir $WORKDIR/myfirstapp. Exiting"; exit 1; }
+  pushd "$script_dir/../../myfirstapp" || { error "Failed to move to dir $script_dir/../../myfirstapp. Exiting"; exit 1; }
   gcloud run deploy myfirstapp \
-  --source .  --allow-unauthenticated \
+  --source . --allow-unauthenticated \
   --set-env-vars="NAME=CND" \
   --quiet 2>/dev/null
   popd || exit 1
