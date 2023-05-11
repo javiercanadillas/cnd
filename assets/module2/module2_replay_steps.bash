2602c4ba5cee3490880e76693311198e15c49ae9
student_04_2d0829b92c7b <student_04_2d0829b92c7b@qwiklabs.net>
2023-05-11 11:25:52 +0000

    Adding Module 2 replay steps

---
+++ assets/module2/module2_replay_steps.bash
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+## Prevent this script from being sourced
+#shellcheck disable=SC2317
+return 0  2>/dev/null || :
+
+# shellcheck disable=SC2128
+script_name=$(basename "$0")
+#shellcheck disable=SC2128,SC2034
+script_dir=$(dirname "$(readlink --canonicalize --no-newline "$0")")
+source "$script_dir/../common/common_bash_libs"
+
+# Check that cloudshell bootstrap has been done
+check_cloudshell_bootstrap() {
+  [[ -f "$HOME/.config/cnd/.cloudshell_bootstrap.done" ]] || { error "Cloudshell bootstrap execution has not been properly registered. Aborting." && exit 1; }
+}
+
+# Check that module1 steps replay has been done
+check_module1_bootstrap() {
+  [[ -f "$HOME/.config/cnd/.module1_steps.done" ]] || { error "Module 1 replay steps have not been properly registered. Aborting." && exit 1; }
+}
+
+## Copy the Docker assets to the app dir
+set_docker_assets_and_deps() {
+  local -r cp_source="$script_dir/../assets/module2"
+  cp -- "$cp_source/Dockerfile" \
+    "$cp_source/.dockerignore" \
+    "$cp_source/requirements.txt" \
+    "$WORKDIR/myfirstapp"
+
+}
+## Set GCP services
+create_gcp_services() {
+  info "Creating necessary GCP services..."
+  gcloud services enable artifactregistry.googleapis.com run.googleapis.com
+  #shellcheck disable=SC2153
+  # Variable should be comming from the environment, checked with cloudshell_bootstrap.bash
+  gcloud artifacts repositories create docker-main --location="$REGION" --repository-format=docker
+  gcloud artifacts repositories list
+}
+
+## Deploy the app to Cloud Run
+deploy_to_cloudrun() {
+  pushd "$WORKDIR/myfirstapp" || { error "Failed to move to dir $WORKDIR/myfirstapp. Exiting"; exit 1; }
+  gcloud run deploy myfirstapp \
+  --source .  --allow-unauthenticated \
+  --set-env-vars="NAME=CND"
+  popd || exit 1
+}
+
+wrap_up() {
+  info "Registering successful replay of module 2 steps..."
+  local -r registry_dir="$HOME/.config/cnd"
+  mkdir -p "$registry_dir"
+  touch "$registry_dir/.module2_steps.done"
+  printfx "--------------------------------------------------------"
+  warning "You must run \"source \$HOME/.bashrc\" before continuing"
+  printfx "--------------------------------------------------------"
+}
+
+main() {
+  info "Replaying Module 2 steps..."
+  check_cloudshell_bootstrap
+  check_module1_bootstrap
+  set_docker_assets_and_deps
+  create_gcp_services
+  deploy_to_cloudrun
+  wrap_up
+}
+
+main "$@"
